8fe1cb053f5de5598b869f7c7d71e1e3c7fa37bf
NickCyrus <nicklemusduque@gmail.com>
2016-11-26 12:17:28 -0500

    new view

--- js/index.js
+++ js/index.js
@@ -1,75 +1,42 @@
-    var pictureSource;    
-    var destinationType;
-
-    document.addEventListener("deviceready",onDeviceReady,false);
-
-    function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
-
-    /*
-    function onPhotoDataSuccess(imageData) {
-      var smallImage = document.getElementById('smallImage');
-      smallImage.style.display = 'block';
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }*/
-
-
-    function onPhotoURISuccess(imageURI) {
-      var largeImage = document.getElementById('largeImage');
-      largeImage.style.display = 'block';
-      largeImage.src = imageURI;
-    }
-
-    function capturePhoto() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,destinationType: destinationType.DATA_URL });
-    }
-
-    function capturePhotoEdit() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL });
-    }
-
-    function getPhoto(source) {
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, destinationType: destinationType.FILE_URI, sourceType: source });
-    }
-
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
-
-    function movePic(file){ 
-        window.resolveLocalFileSystemURI(file, resolveOnSuccess, onFail); 
-    } 
-
-    function resolveOnSuccess(entry){ 
-        var d = new Date();
-        var n = d.getTime();
-        //new file name
-        var newFileName = n + ".jpg";
-        var myFolderApp = "MyAppFolder";
-
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
-        //The folder is created if doesn't exist
-        fileSys.root.getDirectory( myFolderApp,
-                        {create:true, exclusive: false},
-                        function(directory) {
-                            entry.moveTo(directory, newFileName,  successMove, onFail);
-                        },
-                        onFail);
-                        },
-        onFail);
-    }
-
-    function successMove(entry) {
-        sessionStorage.setItem('imagepath', entry.fullPath);
-    }
-
+var App = {
+            btnTakePhoto : 'takePhoto',
+            btnSendPhoto : 'sendPhoto',
+            imgContainer : 'photo',
     
+            init : function(){
+                this.bindEvents();
+            },
+            
+            bindEvents : function(){
+                 var takePhoto = document.getElementById(this.btnTakePhoto);
+                 takePhoto.addEventListener('click', app.takePhoto, false);
+                 var sendPhoto = document.getElementById(this.btnTakePhoto);
+                 sendPhoto.addEventListener('click', app.sendPhoto, false);
+            },
+    
+            takePhoto: function(){
+              navigator.camera.getPicture(app.onPhotoDataSuccess, app.onFail, { quality: 20, allowEdit: true, destinationType:navigator.camera.DestinationType.DATA_URL });
+            },
+    
+            savePhoto: function(){
+                
+            },
+            
+            onPhotoDataSuccess: function(imageData) {
+                var photo = document.getElementById(this.imgContainer);
+                photo.style.display = 'block';
+                photo.src = "data:image/jpeg;base64," + imageData;
+                var sendPhoto = document.getElementById('sendPhoto');
+                sendPhoto.style.display = 'block';
+             },
+ 
+            onFail: function(message) {
+                alert('Failed because: ' + message);
+             }
+    
+            
+        
+    
+}
 
-    function onPhotoDataSuccess(imageURI) { 
-            var imgProfile = document.getElementById('smallImage');
-            imgProfile.src = imageURI;
-            movePic(imageURI);
-    }
-     
+app.init();
