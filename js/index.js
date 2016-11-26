5b35c20e6c33df092e8108fe24db26e8707092e5
NickCyrus <nicklemusduque@gmail.com>
2016-11-26 13:11:29 -0500

    newUddate

--- js/index.js
+++ js/index.js
@@ -1,42 +1,23 @@
-var App = {
-            btnTakePhoto : 'takePhoto',
-            btnSendPhoto : 'sendPhoto',
-            imgContainer : 'photo',
-    
-            init : function(){
-                this.bindEvents();
-            },
-            
-            bindEvents : function(){
-                 var takePhoto = document.getElementById(this.btnTakePhoto);
-                 alert(takePhoto);
-                 takePhoto.addEventListener('click', this.takePhoto, false);
-                 var sendPhoto = document.getElementById(this.btnTakePhoto);
-                 sendPhoto.addEventListener('click', this.sendPhoto, false);
-            },
-    
-            takePhoto: function(){
-              navigator.camera.getPicture(App.onPhotoDataSuccess, App.onFail, { quality: 20, allowEdit: true, destinationType:navigator.camera.DestinationType.DATA_URL });
-            },
-    
-            savePhoto: function(){
-                
-            },
-            
-            onPhotoDataSuccess: function(imageData) {
-                var photo = document.getElementById(this.imgContainer);
-                photo.style.display = 'block';
-                photo.src = "data:image/jpeg;base64," + imageData;
-                var sendPhoto = document.getElementById('sendPhoto');
-                sendPhoto.style.display = 'block';
-             },
- 
-            onFail: function(message) {
-                alert('Failed because: ' + message);
-             }
-    
-            
-        
-    
-}
+    var pictureSource;    
+    var destinationType;
 
+    document.addEventListener("deviceready",onDeviceReady,false);
+
+    function onDeviceReady() {
+        pictureSource   = navigator.camera.PictureSourceType;
+        destinationType = navigator.camera.DestinationType;
+    }
+
+    function onPhotoDataSuccess(imageURI) { 
+            var imgProfile = document.getElementById('smallImage');
+            imgProfile.src = imageURI;
+            movePic(imageURI);
+    }
+    
+    function capturePhoto() {
+        navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: destinationType.DATA_URL });
+    }
+
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
