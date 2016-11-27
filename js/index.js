baebaeb5002be59014573d7a5a6d61329e09d558
NickCyrus <nicklemusduque@gmail.com>
2016-11-27 10:23:37 -0500

    update

--- js/index.js
+++ js/index.js
@@ -2,23 +2,32 @@
     var destinationType;
 
     document.addEventListener("deviceready",onDeviceReady,false);
-
+    
+    
     function onDeviceReady() {
         pictureSource   = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
+        
+        var tapEnabled = true; //enable tap take picture
+        var dragEnabled = true; //enable preview box drag across the screen
+        var toBack = true; //send preview box to the back of the webview
+        var rect = {x: 100, y: 100, width: 200, height:200};
+        cordova.plugins.camerapreview.startCamera(rect, "front", tapEnabled, dragEnabled, toBack)
+
+    
     }
 
     function onPhotoDataSuccess(imageURI) { 
             var imgProfile = document.getElementById('smallImage');
             imgProfile.style.display = 'block';
             imgProfile.src = "data:image/jpeg;base64,"+imageURI;
-            setTimeout(function(){ 
-                document.getElementById('botonera').style.display = 'none';
-                capturaScreen() }, 1000);
+                
+        setTimeout(capturaScreen, 1000);
             
     }
      
     function capturePhoto() {
+        document.getElementById('botonera').style.display = 'none';
         navigator.camera.getPicture(onPhotoDataSuccess, onFail, 
                                     { quality: 50, 
                                       destinationType: destinationType.DATA_URL,
@@ -27,6 +36,7 @@
     }
 
     function onFail(message) {
+      document.getElementById('botonera').style.display = 'block';    
       alert('Failed because: ' + message);
     }
 
